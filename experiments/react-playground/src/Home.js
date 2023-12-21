db983e6de0c7c28d071a3670303b27ae70f7f820
nnall <83522890+nnall@users.noreply.github.com>
2023-12-20 18:21:55 -0600

    scrollTrigger start end settings

--- experiments/react-playground/src/Home.js
+++ experiments/react-playground/src/Home.js
@@ -3,87 +3,159 @@ import "./index.scss";
 
 // typical import
 import gsap from "gsap";
-import { useGSAP } from "gsap";
 
 import Lenis from "@studio-freight/lenis";
 
 import scrollTrigger from "gsap/ScrollTrigger";
-import Flip from "gsap/Flip";
-import Draggable from "gsap/Draggable";
 
 // don't forget to register plugins
-gsap.registerPlugin(scrollTrigger, Draggable, Flip);
+gsap.registerPlugin(scrollTrigger);
 
 const Home = () => {
-  const pRef = useRef(null);
-
-  console.log(pRef);
-
-  //the :before pseudo elements (the dots)
-  //pseudo elements must get a defined variable
-
-  //.to() VS .from()
-
-  //outcome: An element is placed in one area, and moves into  another area/state upon being triggered.
-
-  //you can accomplish this either by setting their 'before' state in the CSS, and using a '.to()' to mobve it into the 'after' position.
-
-  //or you can define the 'before' state inside a .from() and its 'after' state in the CSS
-
-  //.to()
-  // starting from defined CSS, TO something - the argument provided)
-
-  //.from()
-  // starting from (arg provided css), to the defined CSS
-
-  //2 args, the selector to be animated (arg 1) <-- in quotes, and an object (arg 2) of css to be changed on that selected html
-
-  //Smooth Scroll
-
-  // Create a GSAP timeline
-
-  // useGSAP(() => {
-
   useEffect(() => {
-    const el = pRef.current;
-    let tl = gsap.timeline({
-      defaults: { ease: "power4.inOut", duration: 2 },
+    // Step 1: Create a GSAP timeline
+    let tl = gsap.timeline();
+
+    // tl.to("h1", {
+    //   x: 0,
+    //   scrollTrigger: {
+    //     defaults: { ease: "power4.inOut", duration: 2 },
+    //     trigger: ".spacer_1",
+    //     start: "10% 100%",
+    //     end: "55% 50%",
+    //     scrub: true,
+    //     markers: true,
+    //   },
+    // });
+
+    tl.to("p", {
+      x: 0,
       scrollTrigger: {
-        trigger: "p",
-        start: "top center", // when does animation start
-        end: "bottom center", // when does animation end
+        defaults: { ease: "power4.inOut", duration: 2 },
+        trigger: ".spacer_2",
+        start: "40% 100%",
+        end: "55% 50%",
         scrub: true,
         markers: true,
       },
     });
 
-    // gsap.to(el);
+    tl.to(".h2-3", {
+      x: 0,
+      scrollTrigger: {
+        defaults: { ease: "power4.inOut", duration: 2 },
+        trigger: ".spacer_3",
+        start: "35% 100%",
+        end: "55% 50%",
+        scrub: true,
+        markers: true,
+      },
+    });
+
+    tl.to(".h2-4", {
+      x: 0,
+      scrollTrigger: {
+        defaults: { ease: "power4.inOut", duration: 2 },
+        trigger: ".spacer_4",
+        start: "35% 100%",
+        end: "55% 50%",
+        scrub: true,
+        markers: true,
+      },
+    });
+    tl.to(".h2-5", {
+      x: 0,
+      scrollTrigger: {
+        defaults: { ease: "power4.inOut", duration: 2 },
+        trigger: ".spacer_5",
+        start: "35% 100%",
+        end: "55% 50%",
+        scrub: true,
+        markers: true,
+      },
+    });
+    tl.to(".h2-6", {
+      x: 0,
+      scrollTrigger: {
+        defaults: { ease: "power4.inOut", duration: 2 },
+        trigger: ".spacer_6",
+        start: "35% 100%",
+        end: "55% 50%",
+        scrub: true,
+        markers: true,
+      },
+    });
+    tl.to(".h2-7", {
+      x: 0,
+      scrollTrigger: {
+        defaults: { ease: "power4.inOut", duration: 2 },
+        trigger: ".spacer_7",
+        start: "35% 100%",
+        end: "55% 50%",
+        scrub: true,
+        markers: true,
+      },
+    });
+    tl.to(".h2-8", {
+      x: 0,
+      scrollTrigger: {
+        defaults: { ease: "power4.inOut", duration: 2 },
+        trigger: ".spacer_8",
+        start: "35% 100%",
+        end: "55% 50%",
+        scrub: true,
+        markers: true,
+      },
+    });
+    tl.to(".h2-9", {
+      x: 0,
+      scrollTrigger: {
+        defaults: { ease: "power4.inOut", duration: 2 },
+        trigger: ".spacer_8",
+        start: "50% 100%",
+        end: "75% 50%",
+        scrub: true,
+        markers: true,
+      },
+    });
+
+    tl.to(".cat", {
+      x: 0,
+      scrollTrigger: {
+        defaults: { ease: "power4.inOut", duration: 2 },
+        trigger: ".spacer_9",
+        start: "35% 100%",
+        end: "55% 50%",
+        scrub: true,
+        markers: true,
+      },
+    });
+
+    // Step 3: Create a ScrollTrigger to activate the timeline
+
+    // tl.to("h1", { x: 0, duration: 2 });
+    // tl.to("h2", { x: 0, duration: 2 });
+    // tl.to("p", { x: 0, duration: 2 });
+    // tl.to("img", { x: 0, duration: 2 });
+    // tl.to("h1", { x: 0 });
+    // tl.to("h1", { x: 0 });
+
+    // ScrollTrigger.create({
+    //   animation: tl,
+    //   trigger: ".spacer",
+    //   start: "top 50%", // when to start the animation
+    //   end: "top 30%", // when to end the animation
+    //   scrub: true, // smooth scrubbing effect
+    //   markers: true, // for debugging, shows the trigger area
+    // });
+
+    // Step 2: Add animations to the timeline
+    // ScrollTrigger
+    //   .to("h1", { x: 0 })
+    //   .to("h2", { x: 0 })
+    //   .to("p", { x: 0 })
+    //   .to("img", { x: 0 });
   }, []);
-
-  // let tl = gsap.timeline({
-  //   scrollTrigger: {
-  //     trigger: "p",
-  //     start: "top center", // when does animation start
-  //     end: "bottom center", // when does animation end
-  //     scrub: true,
-  //     markers: true,
-  //   },
-  // });
-
-  // tl.to(
-  //   "p",
-  //   {
-  //     x: 1200,
-  //     duration: 3,
-  //   }
-  //   // "-=1"
-  // );
-  // });
-
-  // .to("h1", {
-  //   x: -1200,
-  //   duration: 3,
-  // }).
 
   const lenis = new Lenis();
 
@@ -98,39 +170,15 @@ const Home = () => {
 
   requestAnimationFrame(raf);
 
-  // .to(
-  //   "h1",
-  //   {
-  //     // opacity: 1,
-  //     x: 0,
-  //     duration: 3,
-  //     // lazy: false,
-  //   },
-  //   "-=2"
-  // )
-  // .to(
-  //   "h2",
-  //   {
-  //     x: 0,
-  //     duration: 2.5,
-  //   },
-  //   "-=4"
-  // );
-
-  // Cleanup function to kill the timeline when the component unmounts
-  // return () => {
-  //   tl.kill();
-  // };
-
   return (
     <div className="container">
-      <div className="spacer">
+      <div className="spacer spacer_1">
         <h1>Hey</h1>
       </div>
 
-      <div className="spacer">
+      <div className="spacer spacer_2">
         {/* <h2>We're glad you're here..</h2> */}
-        <p ref={pRef}>
+        <p>
           In the old world, I'd be sitting on a fire escape in some European
           city smoking a cigarette right now, watching people get out into the
           street below. Another day. I don't know that I'd feel any more or less
@@ -143,27 +191,27 @@ const Home = () => {
           you like it? Don't you?
         </p>
       </div>
-      <div className="spacer">
-        <h2>We knew you'd be arriving soon</h2>
+      <div className="spacer spacer_3">
+        <h2 className="h2-3">We knew you'd be arriving soon</h2>
       </div>
-      <div className="spacer">
-        <h2>Don't bother denying it.. </h2>
+      <div className="spacer spacer_4">
+        <h2 className="h2-4">Don't bother denying it.. </h2>
       </div>
-      <div className="spacer">
-        <h2>It's written right on your face</h2>
+      <div className="spacer spacer_5">
+        <h2 className="h2-5">It's written right on your face</h2>
       </div>
-      <div className="spacer">
-        <h2>You were always going to be here</h2>
+      <div className="spacer spacer_6">
+        <h2 className="h2-6">You were always going to be here</h2>
       </div>
-      <div className="spacer">
-        <h2>And now you are</h2>
+      <div className="spacer spacer_7">
+        <h2 className="h2-7">And now you are</h2>
       </div>
-      <div className="spacer">
-        <h2>So just relax and settle down</h2>
-        <h2>I am the captain now</h2>
+      <div className="spacer spacer_8">
+        <h2 className="h2-8">So just relax and settle down</h2>
+        <h2 className="h2-9">I am the captain now</h2>
       </div>
 
-      <div className="spacer">
+      <div className="spacer spacer_9">
         <img src="./img/cat.jpg" alt="" className="cat" />
       </div>
     </div>
